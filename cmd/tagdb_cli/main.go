d06af33fe9fb6ed2ff9fe5f8c7cfaae66eeb8d04
David Rushton <David.Rushton@trayport.com>
2025-11-12 08:07:57 +0000

    Adds basic command routing
    Very much WIP

--- cmd/tagdb_cli/main.go
+++ cmd/tagdb_cli/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"dev.azure.com/trayport/Hackathon/_git/Q/internal/cli"
+)
 
 func main() {
-	fmt.Println("Hola mundo")
+	fmt.Println("cli parsing test")
+	fmt.Println("----------------")
+	fmt.Println()
+
+	cli.NewCommand("good", "a command that should exit 0", goodHandler)
+	cli.NewCommand("bad", "a command that should exit 1", badHandler)
+
+	cli.Run(os.Args)
+}
+
+func goodHandler() int {
+	fmt.Println("good command called")
+	return 0
+}
+
+func badHandler() int {
+	fmt.Println("bad command called")
+	return 1
 }
